8f181959ebe1aa16ee7c0cd19f1adbcfb15f9c7a
Hector Arranz <hector_lastras@hotmail.com>
2021-12-08 12:03:33 +0100

    AoC 2021 Day07 Part1

---
+++ adventofcode/2021/day07/part1.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	var numbers []int
+	var max int
+	for scanner.Scan() {
+		var a string
+		fmt.Sscanf(scanner.Text(), "%s", &a)
+		b := strings.Split(a, ",")
+		for _, x := range b {
+			l, _ := strconv.Atoi(x)
+			numbers = append(numbers, l)
+			if l > max {
+				max = l
+			}
+		}
+	}
+
+	best := -1
+	for i := 0; i < max; i++ {
+		diff := 0
+		for _, v := range numbers {
+			diff += Abs(v - i)
+		}
+
+		if best == -1 || diff < best {
+			best = diff
+		}
+	}
+
+	fmt.Println(best)
+}
