1ec92248cad0fd07d400ee16f6ea81a415a69e2f
Hector Arranz <hector_lastras@hotmail.com>
2021-12-12 11:58:15 +0100

    AoC 2021 Day12 Part2

---
+++ adventofcode/2021/day12/part2.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+)
+
+func isLower(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLower(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func explore(graph map[string][]string, path []string, visited map[string]bool, seen int, smallVisited bool) int {
+	if path[len(path)-1] == "end" {
+		return seen + 1
+	}
+
+	for _, next := range graph[path[len(path)-1]] {
+		if !visited[next] {
+			if isLower(next) {
+				visited[next] = true
+			}
+
+			path = append(path, next)
+			seen = explore(graph, path, visited, seen, smallVisited)
+			path = path[:len(path)-1]
+
+			if isLower(next) {
+				visited[next] = false
+			}
+		} else if !smallVisited {
+			path = append(path, next)
+			seen = explore(graph, path, visited, seen, true)
+			path = path[:len(path)-1]
+		}
+	}
+
+	return seen
+}
+
+func main() {
+	file, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	graph := make(map[string][]string)
+	for scanner.Scan() {
+		line := strings.Split(scanner.Text(), "-")
+		if line[0] != "end" && line[1] != "start" {
+			graph[line[0]] = append(graph[line[0]], line[1])
+		}
+
+		if line[1] != "end" && line[0] != "start" {
+			graph[line[1]] = append(graph[line[1]], line[0])
+		}
+	}
+	fmt.Println(explore(graph, []string{"start"}, make(map[string]bool), 0, false))
+}
