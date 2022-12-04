bc3d7f17dd67e26e4f20369483eb5eb9931f522a
Hector Arranz <hector_lastras@hotmail.com>
2022-12-04 18:36:44 +0100

    AoC 2015 Day06

---
+++ adventofcode/2015/day06/part1.py
@@ -0,0 +1,31 @@
+import re
+import sys
+lines = sys.stdin.read().split("\n")
+
+exp = r"^(turn on|turn off|toggle)\s(\d+),(\d+)\sthrough\s(\d+),(\d+)$"
+def parse(line):
+    r = re.findall(exp, line)
+    acction, x1, y1, x2, y2 = r[0]
+    return acction , [int(x1), int(y1)], [int(x2), int(y2)]
+
+matrix = [[False for j in range(1000)] for i in range(1000)]
+
+for line in lines:
+    action, f, t = parse(line)
+    for i in range(f[0],t[0]+1):
+        for j in range(f[1],t[1]+1):
+            if action == "turn on":
+                matrix[i][j] = True
+            elif action == "turn off":
+                matrix[i][j] = False
+            else:
+                matrix[i][j] = not matrix[i][j]
+
+
+total = 0
+for i in range(len(matrix)):
+    for j in range(len(matrix[0])):
+        if matrix[i][j]:
+            total += 1
+
+print(total)
